bd7f3ec7e2c4f42b6e5740f18110404bb029d27f
xxchan <xxchan22f@gmail.com>
2021-10-19 15:06:33 +0200

    finish hw1

--- peer/impl/handler.go
+++ peer/impl/handler.go
@@ -1,30 +1,210 @@
 package impl
 
 import (
+	"fmt"
+	"math/rand"
+
 	"go.dedis.ch/cs438/transport"
 	"go.dedis.ch/cs438/types"
 )
 
-func chatHandler(m types.Message, p transport.Packet) error {
+type controller struct {
+	node *node
+}
+
+func NewController(node *node) controller {
+	return controller{node: node}
+}
+
+func (c *controller) chatHandler(m types.Message, p transport.Packet) error {
 	return nil
 }
 
-func rumorsHandler(m types.Message, p transport.Packet) error {
-	panic("to implement")
+// TODO: parallel processing. How to handle errors?
+// TODO: Do I need to Ack to myself?
+// TODO: what about using a single mutex to protect status and log?
+func (c *controller) rumorsHandler(m types.Message, p transport.Packet) error {
+	c.node.rumorsMu.Lock()
+	defer c.node.rumorsMu.Unlock()
+
+	rumors := m.(*types.RumorsMessage)
+	newRumor := false
+
+	// Process Rumors
+	for _, rumor := range rumors.Rumors {
+		if rumor.Sequence != c.node.status[rumor.Origin]+1 {
+			// rumor is not expected, ignore it
+			continue
+		}
+		newRumor = true
+
+		// TODO: if we have multiple rumors from the same origin, we may update multiple times
+		if !c.node.neighbors.has(rumor.Origin) {
+			c.node.SetRoutingEntry(rumor.Origin, p.Header.RelayedBy)
+		}
+
+		newPkt := transport.Packet{
+			Header: p.Header,
+			Msg:    rumor.Msg,
+		}
+
+		if err := c.node.conf.MessageRegistry.ProcessPacket(newPkt); err != nil {
+			return fmt.Errorf("failed to process rumor: %v", err)
+		}
+		// It is possible that status has already been updated concurrently
+		c.node.status[rumor.Origin] = rumor.Sequence
+		c.node.rumorsLog[rumor.Origin] = append(c.node.rumorsLog[rumor.Origin], rumor)
+	}
+
+	if p.Header.Source != c.node.getAddr() {
+		// Send back an AckMessage to the source
+		hdr := transport.NewHeader(c.node.getAddr(), c.node.getAddr(), p.Header.Source, 1)
+		ackMsg := types.AckMessage{
+			AckedPacketID: p.Header.PacketID,
+			// TODO: before or after processing rumors?
+			Status: c.node.status,
+		}
+		ackPkt := transport.Packet{
+			Header: &hdr,
+			Msg:    c.node.getMarshalledMsg(&ackMsg),
+		}
+		if err := c.node.conf.Socket.Send(p.Header.Source, ackPkt, timeout); err != nil {
+			return fmt.Errorf("failed to send rumor ack: %v", err)
+		}
+
+		// Send the RumorMessage to another random neighbor in the case where one of the Rumor data in the packet is new
+		if newRumor {
+			if c.node.neighbors.len() == 0 || c.node.neighbors.len() == 1 && c.node.neighbors.has(p.Header.Source) {
+				return nil
+			}
+			neighbor := c.node.neighbors.getRandom()
+			for neighbor == p.Header.Source {
+				neighbor = c.node.neighbors.getRandom()
+			}
+
+			hdr := transport.NewHeader(c.node.getAddr(), c.node.getAddr(), neighbor, 1)
+			p.Header = &hdr
+			if err := c.node.conf.Socket.Send(neighbor, p, timeout); err != nil {
+				return fmt.Errorf("failed to send rumor: %v", err)
+			}
+		}
+	}
+
+	return nil
 }
 
-func statusHandler(m types.Message, p transport.Packet) error {
-	panic("to implement")
+func (c *controller) statusHandler(m types.Message, p transport.Packet) error {
+	c.node.rumorsMu.Lock()
+	defer c.node.rumorsMu.Unlock()
+
+	remoteStatus := *m.(*types.StatusMessage)
+	remoteHasNew := false
+	var remoteMissing []types.Rumor
+
+	c.node.logger.Debug().Msgf("status %v, my status %v", remoteStatus, c.node.status)
+
+	remoteCnt := 0
+	for origin, localSeq := range c.node.status {
+		remoteSeq, ok := remoteStatus[origin]
+		if ok {
+			remoteCnt++
+		}
+		if remoteSeq < localSeq {
+			remoteMissing = append(remoteMissing, c.node.rumorsLog[origin][remoteSeq:]...)
+		} else if remoteSeq > localSeq {
+			remoteHasNew = true
+		}
+	}
+	if len(remoteStatus) > remoteCnt {
+		remoteHasNew = true
+	}
+
+	if remoteHasNew {
+		hdr := transport.NewHeader(c.node.getAddr(), c.node.getAddr(), p.Header.Source, 0)
+		pkt := transport.Packet{
+			Header: &hdr,
+			Msg:    c.node.getMarshalledMsg((*types.StatusMessage)(&c.node.status)),
+		}
+		if err := c.node.conf.Socket.Send(p.Header.Source, pkt, timeout); err != nil {
+			c.node.logger.Trace().Msgf("failed to send status: %v", err)
+		}
+	}
+
+	if remoteMissing != nil {
+		hdr := transport.NewHeader(c.node.getAddr(), c.node.getAddr(), p.Header.Source, 0)
+		pkt := transport.Packet{
+			Header: &hdr,
+			Msg:    c.node.getMarshalledMsg(&types.RumorsMessage{Rumors: remoteMissing}),
+		}
+		if err := c.node.conf.Socket.Send(p.Header.Source, pkt, timeout); err != nil {
+			c.node.logger.Trace().Msgf("failed to send rumors: %v", err)
+		}
+		// Do not wait for the possible ack message
+	}
+
+	if !remoteHasNew && remoteMissing == nil {
+		if rand.Float64() < c.node.conf.ContinueMongering {
+
+			// send a status message to a random neighbor
+			neighbor := c.node.neighbors.getRandom()
+			if neighbor == p.Header.Source && c.node.neighbors.len() == 1 {
+				return nil
+			}
+			for neighbor == p.Header.Source {
+				neighbor = c.node.neighbors.getRandom()
+			}
+
+			hdr := transport.NewHeader(c.node.getAddr(), c.node.getAddr(), neighbor, 0)
+			pkt := transport.Packet{
+				Header: &hdr,
+				Msg:    c.node.getMarshalledMsg((*types.StatusMessage)(&c.node.status)),
+			}
+
+			if err := c.node.conf.Socket.Send(neighbor, pkt, timeout); err != nil {
+				c.node.logger.Trace().Msgf("failed to send status: %v", err)
+			}
+		}
+	}
+
+	return nil
 }
 
-func ackHandler(m types.Message, p transport.Packet) error {
-	panic("to implement")
+func (c *controller) ackHandler(m types.Message, p transport.Packet) error {
+	ack := m.(*types.AckMessage)
+	// Each packet should only be acked once
+	select {
+	case c.node.ackCh[ack.AckedPacketID] <- struct{}{}:
+		close(c.node.ackCh[ack.AckedPacketID])
+		delete(c.node.ackCh, ack.AckedPacketID)
+		c.node.logger.Debug().Msgf("ack %v received", ack.AckedPacketID)
+	default:
+		c.node.logger.Warn().Str("packet_id", ack.AckedPacketID).Msgf("blocking ack")
+	}
+
+	newPkt := transport.Packet{
+		Header: p.Header,
+		Msg:    c.node.getMarshalledMsg(&ack.Status),
+	}
+	if err := c.node.conf.MessageRegistry.ProcessPacket(newPkt); err != nil {
+		return fmt.Errorf("failed to process status: %v", err)
+	}
+	return nil
 }
 
-func emptyHandler(m types.Message, p transport.Packet) error {
-	panic("to implement")
+func (c *controller) emptyHandler(m types.Message, p transport.Packet) error {
+	return nil
 }
 
-func privateHandler(m types.Message, p transport.Packet) error {
-	panic("to implement")
+func (c *controller) privateHandler(m types.Message, p transport.Packet) error {
+	pm := m.(*types.PrivateMessage)
+	if _, ok := pm.Recipients[c.node.getAddr()]; ok {
+		pkt := transport.Packet{
+			Header: p.Header,
+			Msg:    pm.Msg,
+		}
+		if err := c.node.conf.MessageRegistry.ProcessPacket(pkt); err != nil {
+			return fmt.Errorf("failed to process rumor: %v", err)
+		}
+	}
+	return nil
 }
--- peer/impl/mod.go
+++ peer/impl/mod.go
@@ -1,10 +1,14 @@
 package impl
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -22,7 +26,7 @@ import (
 //
 const envLogLevel = "NODELOG"
 
-const defaultLevel = zerolog.InfoLevel
+const defaultLevel = zerolog.Disabled
 
 var level = defaultLevel
 
@@ -55,10 +59,17 @@ const timeout = time.Second * 1
 // NewPeer creates a new peer. You can change the content and location of this
 // function but you MUST NOT change its signature and package location.
 func NewPeer(conf peer.Configuration) peer.Peer {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
 	node := &node{
 		conf:         conf,
-		done:         make(chan struct{}, 1),
 		routingTable: NewConcurrentMap(),
+		status:       make(map[string]uint),
+		ackCh:        make(map[string]chan struct{}),
+		rumorsLog:    make(map[string][]types.Rumor),
+
+		ctx:        ctx,
+		cancelFunc: cancelFunc,
 	}
 	node.logger = zerolog.New(logout).Level(level).With().
 		Timestamp().
@@ -66,16 +77,67 @@ func NewPeer(conf peer.Configuration) peer.Peer {
 		Str("role", "node").
 		Str("myaddr", node.getAddr()).Logger()
 
-	conf.MessageRegistry.RegisterMessageCallback(types.ChatMessage{}, logging(&node.logger)(chatHandler))
-	conf.MessageRegistry.RegisterMessageCallback(types.RumorsMessage{}, logging(&node.logger)(rumorsHandler))
-	conf.MessageRegistry.RegisterMessageCallback(types.StatusMessage{}, logging(&node.logger)(statusHandler))
-	conf.MessageRegistry.RegisterMessageCallback(types.AckMessage{}, logging(&node.logger)(ackHandler))
-	conf.MessageRegistry.RegisterMessageCallback(types.EmptyMessage{}, logging(&node.logger)(emptyHandler))
-	conf.MessageRegistry.RegisterMessageCallback(types.PrivateMessage{}, logging(&node.logger)(privateHandler))
+	ctrl := NewController(node)
+	conf.MessageRegistry.RegisterMessageCallback(types.ChatMessage{}, logging(&node.logger)(ctrl.chatHandler))
+	conf.MessageRegistry.RegisterMessageCallback(types.RumorsMessage{}, logging(&node.logger)(ctrl.rumorsHandler))
+	conf.MessageRegistry.RegisterMessageCallback(types.StatusMessage{}, logging(&node.logger)(ctrl.statusHandler))
+	conf.MessageRegistry.RegisterMessageCallback(types.AckMessage{}, logging(&node.logger)(ctrl.ackHandler))
+	conf.MessageRegistry.RegisterMessageCallback(types.EmptyMessage{}, logging(&node.logger)(ctrl.emptyHandler))
+	conf.MessageRegistry.RegisterMessageCallback(types.PrivateMessage{}, logging(&node.logger)(ctrl.privateHandler))
 
 	// routingTable[myAddr] = myAddr
 	node.AddPeer(conf.Socket.GetAddress())
 	return node
+}
+
+type neighbors struct {
+	sync.RWMutex
+	data []string
+}
+
+func (ns *neighbors) len() int {
+	ns.Lock()
+	defer ns.Unlock()
+
+	return len(ns.data)
+}
+
+func (ns *neighbors) add(neighbor string) {
+	ns.Lock()
+	defer ns.Unlock()
+
+	ns.data = append(ns.data, neighbor)
+}
+
+func (ns *neighbors) delete(neighbor string) {
+	ns.Lock()
+	defer ns.Unlock()
+
+	for i, v := range ns.data {
+		if v == neighbor {
+			ns.data = append(ns.data[:i], ns.data[i+1:]...)
+			return
+		}
+	}
+}
+
+func (ns *neighbors) getRandom() string {
+	ns.RLock()
+	defer ns.RUnlock()
+
+	return ns.data[rand.Intn(len(ns.data))]
+}
+
+func (ns *neighbors) has(neighbor string) bool {
+	ns.Lock()
+	defer ns.Unlock()
+
+	for _, v := range ns.data {
+		if v == neighbor {
+			return true
+		}
+	}
+	return false
 }
 
 // node implements a peer to build a Peerster system
@@ -85,13 +147,40 @@ type node struct {
 	conf         peer.Configuration
 	routingTable ConcurrentMap
 
-	done chan struct{}
+	neighbors neighbors
+	// rumor sequence number, equal to the number of rumors this node has created
+	//
+	// should use atomic operation to get/set
+	seq uint32
+
+	// a node’s view on the system. By “view” we mean all the rumors the
+	// node has processed so far.
+	//
+	// map peer -> the sequence number of the last Rumor processed from that peer.
+	status map[string]uint
+	// TODO: what about store Msg only?
+	rumorsLog map[string][]types.Rumor
+	// protect status & rumorsLog
+	rumorsMu sync.Mutex
+
+	// key: PacketID
+	ackCh map[string]chan struct{}
+
+	ctx        context.Context
+	cancelFunc context.CancelFunc
 
 	logger zerolog.Logger
 }
 
 // Start implements peer.Service
 func (n *node) Start() error {
+	if n.conf.HeartbeatInterval > 0 {
+		go n.heartbeat()
+	}
+	if n.conf.AntiEntropyInterval > 0 {
+		go n.antyEntroy()
+	}
+	n.logger.Info().Msgf("starting ...")
 	go func() {
 		for {
 			pkt, err := n.conf.Socket.Recv(time.Second * 1)
@@ -100,7 +189,7 @@ func (n *node) Start() error {
 			}
 			if err != nil {
 				select {
-				case <-n.done:
+				case <-n.ctx.Done():
 					return
 				default:
 					n.logger.Warn().Msgf("error when receive packet: %v", err)
@@ -135,7 +224,7 @@ func (n *node) Start() error {
 			}
 
 			select {
-			case <-n.done:
+			case <-n.ctx.Done():
 				return
 			default:
 			}
@@ -147,12 +236,8 @@ func (n *node) Start() error {
 
 // Stop implements peer.Service
 func (n *node) Stop() error {
-	select {
-	case n.done <- struct{}{}:
-		return nil
-	default:
-		return fmt.Errorf("already stopped")
-	}
+	n.cancelFunc()
+	return nil
 }
 
 // Unicast implements peer.Messaging
@@ -174,7 +259,82 @@ func (n *node) Unicast(dest string, msg transport.Message) error {
 
 // Broadcast implements peer.Messaging
 func (n *node) Broadcast(msg transport.Message) error {
-	panic("not implemeted")
+	// TODO: If error occurs, do we need to decrement?
+	atomic.AddUint32(&n.seq, 1)
+	rumors := types.RumorsMessage{Rumors: []types.Rumor{
+		{
+			Origin:   n.getAddr(),
+			Sequence: uint(atomic.LoadUint32(&n.seq)),
+			Msg:      &msg,
+		},
+	}}
+
+	// execute the message "locally" first, so that my status is updated
+	hdr := transport.NewHeader(n.getAddr(), n.getAddr(), n.getAddr(), 1)
+	pkt := transport.Packet{
+		Header: &hdr,
+		Msg:    n.getMarshalledMsg(&rumors),
+	}
+	if err := n.conf.MessageRegistry.ProcessPacket(pkt); err != nil {
+		return fmt.Errorf("failed to process rumor locally: %v", err)
+	}
+
+	// sends the rumor to a random neighbor.
+	if n.neighbors.len() == 0 {
+		n.logger.Warn().Msgf("has none neighbors")
+		return nil
+	}
+	neighbor := n.neighbors.getRandom()
+	hdr = transport.NewHeader(n.getAddr(), n.getAddr(), neighbor, 1)
+	pkt.Header = &hdr
+
+	ackCh := make(chan struct{}, 1)
+	n.ackCh[pkt.Header.PacketID] = ackCh
+
+	if err := n.conf.Socket.Send(neighbor, pkt, timeout); err != nil {
+		return fmt.Errorf("failed to send rumor: %v", err)
+	}
+	n.logger.Debug().Str("packet_id", pkt.Header.PacketID).Msgf("rumor is sent to %v", neighbor)
+
+	// wait for ack. If no ack, send to another neighbor
+	go func() {
+		for acked := false; !acked; {
+			n.logger.Debug().Msgf("broadcast wait for ack %v, neighbor %v", pkt.Header.PacketID, pkt.Header.Destination)
+
+			if n.conf.AckTimeout == 0 {
+				<-ackCh
+				acked = true
+			} else {
+				select {
+				case <-ackCh:
+					acked = true
+				case <-time.After(n.conf.AckTimeout):
+					// TODO: in this case, how to gc (delete map entry)?
+					// send the RumorMessage to another random neighbor
+					if n.neighbors.len() == 1 {
+						n.logger.Warn().Msgf("neighbor did not ack, but has only 1 neighbor")
+						return
+					}
+					newNeighbor := n.neighbors.getRandom()
+					for newNeighbor == neighbor {
+						newNeighbor = n.neighbors.getRandom()
+					}
+					neighbor = newNeighbor
+					hdr = transport.NewHeader(n.getAddr(), n.getAddr(), neighbor, 1)
+					pkt.Header = &hdr
+
+					ackCh := make(chan struct{}, 1)
+					n.ackCh[pkt.Header.PacketID] = ackCh
+
+					if err := n.conf.Socket.Send(neighbor, pkt, timeout); err != nil {
+						n.logger.Error().Msgf("failed to send rumor: %v", err)
+					}
+				}
+			}
+		}
+	}()
+
+	return nil
 }
 
 // AddPeer implements peer.Service
@@ -195,8 +355,12 @@ func (n *node) SetRoutingEntry(origin, relayAddr string) {
 	relayAddr = strings.TrimSpace(relayAddr)
 	if relayAddr == "" {
 		n.routingTable.Delete(origin)
+		n.neighbors.delete(origin)
 	} else {
 		n.routingTable.Set(origin, relayAddr)
+		if origin == relayAddr && origin != n.getAddr() {
+			n.neighbors.add(origin)
+		}
 	}
 }
 
@@ -204,11 +368,73 @@ func (n *node) getAddr() string {
 	return n.conf.Socket.GetAddress()
 }
 
+func (n *node) heartbeat() {
+	msg := transport.Message{
+		Type: types.EmptyMessage{}.Name(),
+	}
+
+	for {
+		// TODO: cannot call Broadcast?
+		err := n.Broadcast(msg)
+		if err != nil {
+			n.logger.Trace().Msgf("heartbeat failed")
+		}
+
+		select {
+		case <-n.ctx.Done():
+			return
+		default:
+		}
+		time.Sleep(n.conf.HeartbeatInterval)
+	}
+}
+
+// Convert types.Message (must be pointer) to *transport.Message.
+func (n *node) getMarshalledMsg(msg types.Message) *transport.Message {
+	logger := n.logger.With().CallerWithSkipFrameCount(3).Logger() // report the caller of this function
+	m, err := n.conf.MessageRegistry.MarshalMessage(msg)
+	if err != nil {
+		logger.Error().Msgf("failed to marshall message: %v", err)
+		// TODO: or return nil/error?
+		return &transport.Message{
+			Type:    msg.Name(),
+			Payload: nil,
+		}
+	}
+	return &m
+}
+
+func (n *node) antyEntroy() {
+	for n.neighbors.len() == 0 {
+		time.Sleep(n.conf.AntiEntropyInterval)
+	}
+	pkt := transport.Packet{}
+	for {
+		neighbor := n.neighbors.getRandom()
+		hdr := transport.NewHeader(n.getAddr(), n.getAddr(), neighbor, 1)
+		pkt.Header = &hdr
+		n.rumorsMu.Lock()
+		pkt.Msg = n.getMarshalledMsg((*types.StatusMessage)(&n.status))
+		n.rumorsMu.Unlock()
+		if err := n.conf.Socket.Send(neighbor, pkt, timeout); err != nil {
+			n.logger.Trace().Msgf("failed to send anti-entropy status: %v", err)
+		}
+
+		select {
+		case <-n.ctx.Done():
+			return
+		default:
+		}
+		time.Sleep(n.conf.AntiEntropyInterval)
+	}
+}
+
 // logging is a utility function that adds logging in a handler
 func logging(logger *zerolog.Logger) func(registry.Exec) registry.Exec {
 	return func(next registry.Exec) registry.Exec {
 		return func(m types.Message, p transport.Packet) error {
-			logger.Info().
+			newlogger := logger.With().CallerWithSkipFrameCount(2).Logger()
+			newlogger.Info().
 				Str("source", p.Header.Source).
 				Str("packet_id", p.Header.PacketID).
 				Str("message_type", m.Name()).
